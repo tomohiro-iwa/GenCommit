e4e41180d1891d5469335526540faaabb2eeb442
tomohiro-iwa <tomo_develop_singularity_111@ezweb.ne.jp>
2018-07-26 18:47:42 +0900

    update split and delete katakana data set

--- log2corpus.py
+++ log2corpus.py
@@ -1,49 +1,55 @@
 import glob
+import re
+import unicodedata
 
-replace_list=(
-("\n"," <BR> "),
-("(","( "),
-(")"," )"),
-("."," . "),
-("/"," / "),
-(","," , ")#正規表現　a-zなどなど
-)
-
+## 改行　+++ ---数値タグ化
+## filter
+### 大文字小文字統一　ステミング
+patarn = re.compile(r"([a-zA-Z0-9]*)")
+katakana = re.compile(r"[ア-ン]")
 def corpus_filter(corpus_str):
-	#for line in corpus_str.split("\n"):
-	for i in replace_list:
-		corpus_str = corpus_str.replace(i[0],i[1])
-	return corpus_str
+	return patarn.sub(r" \1 ",corpus_str)
 			
 def line_generator(gitlog):
+	result = []
 	corpus_str = ""
 	is_first = True
 	for line in gitlog:
 		if line[0:7] == "!!!!!!!":
-			if not is_first:
-				yield corpus_filter(corpus_str)
-				corpus_str = ""
-			else:
+			result.append( line[7:-1] )
+			if is_first:
 				is_first = False
-			yield line[7:-1]
+			else:
+				result.append(corpus_filter(corpus_str))
+				yield result
+				result = []
+				corpus_str = ""
 		else:
 			corpus_str += line
-			
-def make_corpus(repo_name,log_dir,result_dir):
 
+def have_trush(string):
+	name = unicodedata.name(string) 
+	if "CJK UNIFIED" in name \
+	or "HIRAGANA" in name \
+	or "KATAKANA" in name:
+		return True
+	else:
+		return False
+
+def make_corpus(repo_name,log_dir,result_dir):
 	print(log_dir+repo_name+"mygitlogdata.txt")
+
 	gitlog = open(log_dir+repo_name+"mygitlogdata2.txt")
 	commit = open(result_dir+repo_name+"commit_message.txt","a")
 	diff = open(result_dir+repo_name+"diff.txt","a")
 
 	log_lines = line_generator(gitlog)
-	is_odd = True
 	for line in log_lines:
-		if is_odd:
-			commit.write(line+"\n") 
-		else:
-			diff.write(line+"\n")
-		is_odd = not is_odd
+
+		if katakana.search(line[1]):
+			continue
+		commit.write(line[0]+"\n") 
+		diff.write(line[1]+"\n")
 
 	commit.close()
 	diff.close()
@@ -60,8 +66,9 @@ def main():
 			make_corpus(repo_name,log_dir,result_dir)
 		except:
 			print("error")
+		#make_corpus(repo_name,log_dir,result_dir)
 		print("end")
 
-		
+
 if __name__ == "__main__":
 	main()

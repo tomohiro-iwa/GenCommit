e3afc681f012ab5c61957b85bdbed515a5b15132
Titan111 <tomo_develop_singularity_111@ezweb.ne.jp>
2018-07-28 15:46:40 +0900

    complete get_top_github_ranking

--- get_top_github_ranking.py
+++ get_top_github_ranking.py
@@ -1,23 +1,29 @@
 import urllib.request as url_req
 from bs4 import BeautifulSoup
+from joblib import Parallel, delayed
+import time
+
 
 def ranking_page(page):
 	l = "https://github.com/search?l=Java&p="
 	r = "&q=stars%3A%3E1&s=stars&type=Repositories"
 	return l+str(page)+r
 
-def get_repo_list(soup):
+def get_repo_list(url):
 	repo_list = []
+	html = url_req.urlopen(url)
+	soup = BeautifulSoup(html,"lxml")
 	for tag in soup.select("a.v-align-middle"):
 		print(tag["href"])
 	return repo_list
 		
 def main():
-	for i in range(1,101):
-		html = url_req.urlopen(ranking_page(i))
-		soup = BeautifulSoup(html,"lxml")
-		for repo in get_repo_list(soup):	
+	url_list = [ranking_page(i) for i in range(1,101)]
+	for url in url_list:
+		repo_list = get_repo_list(url)
+		for repo in repo_list:
 			print(repo)
+		time.sleep(10)
 
 if __name__ == "__main__":
 	main()

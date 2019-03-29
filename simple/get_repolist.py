8d9c2059e0643c4f1b7382cc8ba759dbae4a2860
tomohiro-iwa <tomo_develop_singularity_111@ezweb.ne.jp>
2019-03-29 17:12:48 +0900

    add simple

--- simple/get_repolist.py
+++ simple/get_repolist.py
@@ -1,8 +1,6 @@
 import urllib.request as url_req
 from bs4 import BeautifulSoup
-from joblib import Parallel, delayed
 import time
-
 
 def ranking_page(page):
 	l = "https://github.com/search?l=Java&p="
@@ -14,16 +12,16 @@ def get_repo_list(url):
 	html = url_req.urlopen(url)
 	soup = BeautifulSoup(html,"lxml")
 	for tag in soup.select("a.v-align-middle"):
-		print(tag["href"])
+		repo_list.append(tag["href"])
 	return repo_list
 		
 def main():
-	url_list = [ranking_page(i) for i in range(1,101)]
+	url_list = [ranking_page(i) for i in range(1,1001)]
 	for url in url_list:
 		repo_list = get_repo_list(url)
 		for repo in repo_list:
 			print(repo)
-		time.sleep(10)
+		time.sleep(30)
 
 if __name__ == "__main__":
 	main()
